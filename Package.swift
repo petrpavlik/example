c075139343b8c04e9e98e1e70acfc5b30fc48f9b
Petr Pavlik <petrpavlik@me.com>
2016-10-05 00:07:28 +0200

    add swiftl as a dependency

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 18),
+        .Package(url: "https://github.com/petrpavlik/Swiftl.git", majorVersion: 0)
     ],
     exclude: [
         "Config",
